d03440aa2c9230ccd0d5fe890e91d8c4cf59fa07
AJ <aj.nice@kakaocorp.com>
2022-05-26 16:33:15 +0900

    form이벤트 추가 , todos에 넣기

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import { log } from './utils';
 import './todos.css';
 
-log('1111111111');
+const todos = [];
+
+//
+// 폼,input 입력 이벤트 추가하기
+// 입력했을때 todo추가
+const $form = document.querySelector('.new-task');
+const $input = document.querySelector('.new-task > input');
+
+const checkTodo = (event) => {
+  event.preventDefault();
+  console.log($input.value);
+
+  // todo 추가
+  const todo = { title: $input.value, isDone: false };
+  todos.push(todo);
+  console.log(todos);
+
+  // input 초기화
+
+  // todos 출력
+};
+
+$form.addEventListener('submit', checkTodo);
+
+// todos 뿌려주기
